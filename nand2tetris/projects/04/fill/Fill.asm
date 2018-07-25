54832f18036c4f7ba9c347cabfce060287b5302d
kishan <manikishanghantasala@gmail.com>
2018-07-25 18:38:54 +0530

    completed fill and mult asm

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,49 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(BEGIN)
+	@SCREEN
+	D=A
+
+	@cursor // this register will carry the current address of pixel we are goin to manipulate
+	M=D
+
+(LOOP)
+	@KBD // set keyboard register RAM[24576]
+	D=M // getting thevalue returned by kbd(0 or non-0)
+	
+	@BLANK //points to method supposed to make screen white
+	D;JEQ // jump to  blank if kbd == 0
+
+	@FILL // points to the method to make screen black 
+	0;JMP  //both are working
+//TODO: [! figure out difference between them]
+	//D;JNE
+	
+(BLANK)
+	@cursor 
+	A=M // setting the address ofcursor to the destination
+	M=0 //and set it's value to 0 , bcz we should print white
+	@NEXT  // method to progress to next step
+	0;JMP
+
+(FILL)
+	@cursor
+	A=M // setting the address ofcursor to the destination
+	M=-1 //and set it's value to -1(1111111111111111) , bcz we should print black
+	
+	@NEXT
+	0;JMP
+(NEXT)
+	@cursor 
+	D=M+1 // increase address to where to go by 1
+	M=D
+	
+	@KBD
+	D=A-D // such that it doesnt exceed the screen region in memory and overflow happens
+	
+	@BEGIN
+	D;JEQ
+	@LOOP
+	0;JMP
--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -7,3 +7,56 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R1
+D=M
+
+@i
+M=D
+
+@mul
+M=0
+
+(LOOP)
+@i
+D=M
+
+@END
+D;JEQ
+
+@R0
+D=M
+
+@mul
+M = D+M
+
+@i
+M=M-1
+
+@LOOP
+0;JMP
+
+(END)
+
+@mul
+D=M
+
+@R2
+M=D
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
